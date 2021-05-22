472e3dedf419bbbc8d4733fc610c18ac3829c9ae
Puneet Wasan <puneet_wasan@yahoo.com>
2021-05-22 13:12:37 +0530

    feat: textbox and div for input&output

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 'use strict';
+const button= document.querySelector('#btn-primary');
+const textArea = document.querySelector('textarea')
+const outputArea= document.querySelector('.output');
+button.addEventListener('click', function(){
+        outputArea.textContent=textArea.value;
+})
