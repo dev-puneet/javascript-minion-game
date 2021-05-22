0e03f18a7258d6fcbbf37c51c17292be965b725a
Puneet Wasan <puneet_wasan@yahoo.com>
2021-05-22 14:42:12 +0530

    refactor: added the api

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 'use strict';
+
 const button= document.querySelector('#btn-primary');
 const textArea = document.querySelector('textarea')
 const outputArea= document.querySelector('.output');
-button.addEventListener('click', function(){
-        outputArea.textContent=textArea.value;
-})
+
+const serverUrl= "https://api.funtranslations.com/translate/minion.json";
+
+const createURL = function(text){
+        return serverUrl+ "?" + "text=" + text
+};
+
+const errorHandler= function(error){
+        alert("something went wrong with the server");
+}
+
+const clickhandler = function(){
+        var txtInput = textArea.value; //input
+        //processing , callback function
+        fetch(createURL(txtInput))
+        .then(response=>response.json())
+        .then(json=> {
+                var translatedText=json.contents.translated;
+                outputArea.innerText = translatedText; //output
+        })
+        .catch(errorHandler)
+}
+
+button.addEventListener('click', clickhandler);
